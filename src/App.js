f7c5266c81106e87f692c39520ae98086d22213c
Rohit9Mankar <rohitmankar0079@gmail.com>
2023-03-09 17:51:32 +0530

    cancel button for recurring api request

--- src/App.js
+++ src/App.js
@@ -5,38 +5,83 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-const [isLoading,setIsLoading]=useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setIsError] = useState(null);
+ let intervalId;
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
+    setIsError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/fils/");
+      if (!response.ok) {
+        
+   intervalId= setInterval(()=>{
+       fetch("https://swapi.dev/api/fils/")
+    },5000);
+    
+        throw new Error('Something went Wrong, Retrying..');
+       
+      }
+     
+      const data = await response.json();
 
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformed = await data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
+      
 
-    })
-    setMovies(transformed);
+      const transformed = await data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+
+      })
+      setMovies(transformed);
+
+
+    }
+    
+    catch (error) {
+    
+      setIsError(error.message)
+    }
+
     setIsLoading(false);
   }
 
+  function removeHandler(){
+   return ()=>clearInterval(intervalId);
+  }
+
+ 
+
+
+
+  let content=<p>Found no movies</p>
+
+  if(movies.length > 0){
+    content=<MoviesList movies={movies} />
+  }
+  if(error){
+    content=<p>{error}</p>
+  }
+  if(isLoading){
+    content=<p>Loading...</p>
+  }
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={removeHandler}>Cancel</button>
       </section>
       <section>
-        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length===0 && <p>Found no Movies</p>}
-        {isLoading && <p> Loading... </p>}
+        {content}
       </section>
     </React.Fragment>
   );
 }
+ 
+
 
 export default App;
